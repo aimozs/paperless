e03366632cdede7a640f5fc6e81da7dc5a70b71d
Matt <mattwerth@mattwerth.com>
2014-10-21 16:29:17 +1100

    engagement agreement and other tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021020246) do
+ActiveRecord::Schema.define(version: 20141021042813) do
 
   create_table "exercises", force: true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141021020246) do
     t.text     "injuries"
     t.string   "gender"
     t.text     "engagement"
+    t.boolean  "agreed"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
